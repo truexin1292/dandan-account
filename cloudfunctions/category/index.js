8091c76d2b99f153a8220934875c7ee768ef32da
guozf2018 <529597521@qq.com>
2019-12-28 17:56:22 +0800

    账单分类方法添加
    添加更新父分类和删除父分类的方法

--- cloudfunctions/category/index.js
+++ cloudfunctions/category/index.js
@@ -16,7 +16,7 @@ exports.main = async (event) => {
   const {
     id, categoryName, categoryIcon, description, flow,
     type, parentId, isSelectable, ids,
-  } = event;
+  } = event; 
 
   const _ = db.command;
 
@@ -122,6 +122,126 @@ exports.main = async (event) => {
         message: '操作成功',
       };
     }
+
+    /**
+     * 更新账单父分类记录 by 账单分类ID
+     */
+    if (event.mode === 'updateParentCategoryById') {
+      const updateRes = await db.collection('DANDAN_NOTE_CATEGORY').doc(id)
+        .update({
+          data: {
+            categoryName,
+            categoryIcon,
+            description,
+            flow: Number(flow),
+            type: Number(type),
+            isSelectable,
+          },
+        });
+      return {
+        code: 1,
+        data: updateRes,
+        message: '操作成功',
+      };
+    }
+
+    /**
+     *  删除账单分类 by 账单分类ID
+     */
+    if (event.mode === 'deleteCategoryById') {
+      let afterCategoryId = 'others_sub'
+      if (Number(flow) === 1) {
+        afterCategoryId = 'income_others'
+      }
+
+      const updateCategoryRes = db.collection('DANDAN_NOTE_CATEGORY').doc(id)
+        .update({
+          data: {
+            isDel: true,
+          },
+        });
+
+      //更新该用户某个子分类下的所有未删除账单分类改为杂项
+      const updateRes = db.collection('DANDAN_NOTE')
+        .where({
+          categoryId: id,
+          isDel: false,
+          openId : wxContext.OPENID,
+        })
+        .update({
+          data: {
+            categoryId: afterCategoryId,
+          },
+        });
+      return Promise.all([updateCategoryRes, updateRes]).then((result) => {
+        return {
+          code: 1,
+          data: result,
+          message: '操作成功',
+        };
+      }).catch((error) => {
+        console.log(error)      // 失败了，打出 '失败'
+      })
+    }
+
+    /**
+     *  删除账单分类 by 账单分类ID AND 所有账单
+     */
+    if (event.mode === 'deleteCategoryByIdAndAll') {
+      //获取被删除父分类下， 所有子分类的ID集合
+      const categoryRes = await db.collection('DANDAN_NOTE_CATEGORY')
+        .where({
+          data: {
+            parentId: id,
+            isDel: true,
+          },
+        });
+      //逻辑删除父分类
+      const updateParentCategoryRes = db.collection('DANDAN_NOTE_CATEGORY').doc(id)
+        .update({
+          data: {
+            isDel: true,
+          },
+        });
+      //逻辑删除子分类
+      const updateCategoryRes = db.collection('DANDAN_NOTE_CATEGORY')
+        .where({
+          parentId: id,
+          isDel: false,
+          openId: wxContext.OPENID,
+        })
+        .update({
+          data: {
+            isDel: true,
+          },
+        });
+      //逻辑删除属于该子分类的账单
+      const categoryIdArray = [];
+      categoryRes.forEach(function (val, index, arr) {
+        categoryIdArray.push(val._id);
+      });
+      const noteUpdateRes = db.collection('DANDAN_NOTE')
+        .where({
+          categoryId: _.in(categoryRes),
+          isDel: false,
+          openId: wxContext.OPENID,
+        })
+        .update({
+          data: {
+            isDel: true,
+          },
+        });
+      return Promise.all([updateParentCategoryRes, updateCategoryRes, noteUpdateRes]).then((result) => {
+        return {
+          code: 1,
+          data: result,
+          message: '操作成功',
+        };
+      }).catch((error) => {
+        console.log(error)      // 失败了，打出 '失败'
+      })
+    }
+
   } catch (e) {
     return {
       code: -1,
